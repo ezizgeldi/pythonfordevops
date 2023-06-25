6d03337a6080a700115ae701bf4bdd0dd07f62dc
ezizgeldi <fc.chelsea2013@mail.ru>
2023-06-26 00:46:47 +0300

    learning str type

---
+++ sequence.py
@@ -0,0 +1,23 @@
+# sequence = ['1', '0', '2']
+
+
+# print(sequence[0])
+# print(sequence[:1])
+# print(sequence[1:])
+
+
+# count = []
+
+
+# for i in range(10):
+#     x = i*10
+#     count.append(x)
+
+
+# print (count)
+
+
+count = [i*i for i in range(10) if i%2==0]
+
+
+print(count)
