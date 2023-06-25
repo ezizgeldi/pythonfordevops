16efc5f8a307b7f73eb9e5e0e6828fb68719774d
ezizgeldi <fc.chelsea2013@mail.ru>
2023-06-25 21:53:22 +0300

    Learning the basic math command

--- basethings.py
+++ basethings.py
@@ -1,1 +1,28 @@
-print("Learning git")
+# count = 10
+
+# for i in range(count):
+#     x = i*2
+#     if x == 6:
+#         continue
+#     print(x)
+ 
+
+# counts = 0
+
+# while counts < 10:
+#     counts += 1
+#     print(counts)
+
+
+
+# thinkers = ['ibn sina', 'platon']
+
+# while True:
+#     try:
+#         thinker = thinkers.pop()
+#         print(thinker)
+
+#     except IndexError as e:
+#         print("not enaght items")
+#         print(e)
+#         break
