25ee95fefe9bf145033ed4fd368e7f353d750db2
ezizgeldi <fc.chelsea2013@mail.ru>
2023-06-25 22:35:54 +0300

    the base concept of oop in python

---
+++ oop_objects.py
@@ -0,0 +1,14 @@
+class FancyCar():
+    wheels = 4
+    color = 'red'
+
+    def speed(self):
+        print('car driving  so fast')
+
+
+my_car = FancyCar()
+
+print(my_car.color)
+print(my_car.wheels)
+my_car.speed()
+
