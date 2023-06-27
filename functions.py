65695a786c897ffd3ff9e186e6f139b7216e125a
ezizgeldi <fc.chelsea2013@mail.ru>
2023-06-28 02:39:30 +0300

    the end  of function

--- functions.py
+++ functions.py
@@ -1,20 +1,26 @@
-### fucntion is a mechanism for encapsulating  a block of code ###
+# ### fucntion is a mechanism for encapsulating  a block of code ###
 
-def position(first, second):
+# def position(first, second):
 
-    print(f'this is the {first} argument')
-    print(f'this is the {second} argument')
+#     print(f'this is the {first} argument')
+#     print(f'this is the {second} argument')
 
-position(1, 2)
-
-
-def keyword(first=1,second=2):
-    print(f'this is the {first} argument')
-    print(f'this is the {second} argument')
-
-keyword()
+# position(1, 2)
 
 
+# def keyword(first=1,second=2):
+#     print(f'this is the {first} argument')
+#     print(f'this is the {second} argument')
 
+# keyword()
 
+def second(input):
+    return input*3
+
+def first(input):
+    return input*45
+
+functions = [second, first]
+for func in functions:
+    print(func(3))
 
