93caa816de3afd0f815a4c122f7b017e7615ae42
ezizgeldi <fc.chelsea2013@mail.ru>
2023-06-27 15:32:49 +0300

    Function is a machanism to encapsulating a  block of code

---
+++ functions.py
@@ -0,0 +1,20 @@
+### fucntion is a mechanism for encapsulating  a block of code ###
+
+def position(first, second):
+
+    print(f'this is the {first} argument')
+    print(f'this is the {second} argument')
+
+position(1, 2)
+
+
+def keyword(first=1,second=2):
+    print(f'this is the {first} argument')
+    print(f'this is the {second} argument')
+
+keyword()
+
+
+
+
+
--- sequence.py
+++ sequence.py
@@ -1,23 +1,24 @@
 # sequence = ['1', '0', '2']
-
-
 # print(sequence[0])
 # print(sequence[:1])
 # print(sequence[1:])
 
 
 # count = []
-
-
 # for i in range(10):
 #     x = i*10
 #     count.append(x)
-
-
 # print (count)
 
+# count = [i*i for i in range(10) if i%2==0]
+# print(count)    
 
-count = [i*i for i in range(10) if i%2==0]
+# input = ' fucking space'
+# print(input)
+# print(input.strip())
+# print(input.rstrip())
+# print(input.lstrip())
 
+text = 'Many had a little lamb'
 
-print(count)    
+print(text.split())
